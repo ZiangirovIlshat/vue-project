5f1deed0c163e7eef9b0780c76d26b0c5260fd3d
Ilshat <48647878+ZiangirovIlshat@users.noreply.github.com>
2024-02-25 15:31:03 +0300

    Обновление стилей
    Добавил обнуление стилей. Подключил шрифты и добавил стили для шрифтов в корневой компонент.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="wrapper">
+
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    export default {
+        
     }
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: "Inter", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+    font-variation-settings: "slnt" 0;
   }
-}
 </style>

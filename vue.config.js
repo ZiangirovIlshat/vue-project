eb02f17e583f00b53e5e888918ca19164f729632
Ilshat <48647878+ZiangirovIlshat@users.noreply.github.com>
2024-02-26 03:03:16 +0300

    поправил конфиг

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const webpack = require('webpack');
 
 module.exports = defineConfig({
-  // publicPath: "/vue-project/dist/",
+  publicPath: "/vue-project/dist/",
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({

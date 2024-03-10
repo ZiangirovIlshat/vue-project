6dcfc16c5ffcb2f68a707875c17c67dcf1d0274b
Ilshat <48647878+ZiangirovIlshat@users.noreply.github.com>
2024-03-10 23:42:08 +0300

    обавил компонент загрузчик

---
+++ src/components/loader.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="loader">
+        <div
+            class="loader__spinner" 
+            :style="{
+                'width' : size + 'px',
+                'height' : size + 'px',
+                'border' : '2px solid ' + color,
+                'border-bottom-color': 'transparent',
+            }"
+        >
+        </div>
+    </div>
+</template>
+
+<script>
+    import { defineComponent } from "vue"
+
+    export default defineComponent({
+        name: "loader",
+        props: {
+            color: {
+                default: '#31AE54',
+            },
+            size: {
+                default: '28',
+            }
+        },
+        computed: {
+            cssVars() {
+                return {
+                    '--color' : this.color,
+                    '--size' : this.size,
+                }
+            }
+        },
+    })
+</script>
+
+<style lang="scss" scoped>
+.loader {
+    padding: 5px;
+    display: inline-block;
+
+    &__spinner {
+        animation: rotation 1s linear infinite;
+        border-radius: 50%;
+
+        @keyframes rotation {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+    }
+}
+
+</style>

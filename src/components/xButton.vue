ebf3ffc2426ada1c50ce308a83245b3b2d9ee0ae
Ilshat <48647878+ZiangirovIlshat@users.noreply.github.com>
2024-03-20 14:43:23 +0300

    Сверстал страницу для авторизации

--- src/components/xButton.vue
+++ src/components/xButton.vue
@@ -1,43 +1,92 @@
 <template>
-    <button 
-        class="x-button"
-        @click="this.$emit('handleClick')"
-    >
-        <span class="x-button__btn-text">
-            <slot ref="btn"></slot>
-        </span>
-    </button>
+  <button
+    class="x-button"
+    :class="[type, { __loading: loading }]"
+    @click="this.$emit('handleClick')"
+  >
+    <span class="x-button__btn-text">
+      <loader v-if="loading" :color="'#fff'" :size="24" />
+      <slot v-else />
+    </span>
+  </button>
 </template>
 
 <script>
-    import { defineComponent } from "vue"
+import { defineComponent } from "vue";
 
-    export default defineComponent({
-        name: "xButton",
-        emits: ['handleClick'],
+import loader from "../components/loader";
 
-        props: {
-            hoverText: String
-        },
-    })
+export default defineComponent({
+  name: "xButton",
+
+  components: {
+    loader,
+  },
+
+  emits: ["handleClick"],
+
+  props: {
+    type: {
+      type: String,
+      default: "green",
+    },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
 </script>
 
 <style lang="scss" scoped>
-    .x-button {
-        background-color: #31AE54;
-        color: #fff;
-        padding: 12px 52px;
-        font-size: 16px;
-        border-radius: 5px;
-        cursor: pointer;
-        transition: .2;
-        border: none;
-    }
+.x-button {
+  color: #fff;
+  padding: 12px 52px;
+  font-size: 16px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.2;
+  border: none;
 
-    @media(hover: hover) and (pointer: fine) {
-        .x-button:hover {
-            background: #9E9E9E;
-        }
-    }
+  @media (max-width: 576px) {
+    padding: 5px 25px;
+    border-radius: 3px;
+  }
 
+  &.green {
+    background-color: #31ae54;
+  }
+
+  &.gray {
+    background-color: #9e9e9e;
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      &.green {
+        background-color: #9e9e9e;
+      }
+
+      &.gray {
+        background-color: #31ae54;
+      }
+    }
+  }
+
+  &.__loading {
+    padding: 8px 52px;
+    background-color: #9e9e9e;
+    @media (hover: hover) and (pointer: fine) {
+      &:hover {
+        background-color: #9e9e9e;
+      }
+    }
+  }
+
+  &__btn-text {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+}
 </style>
---
+++ src/pages/auth.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="auth-page">
+    <div class="auth-page__body">
+      <div class="auth-page__text-content">
+        <logo />
+        <p>More than just one repository.<br>This is our digital world.</p>
+        <xButton>
+          Authorize with github
+          <svg viewBox="0 0 24 24" width="24" height="24">
+            <use href="../assets/sprite.svg#git" x="0" y="0"></use>
+          </svg>
+        </xButton>
+      </div>
+      <div class="auth-page__img">
+        <template v-if="screenWidth >= 768">
+          <img src="../assets/device.png" alt="Gitogram">
+        </template>
+        <template v-if="screenWidth < 768 && screenWidth > 576">
+          <img src="../assets/device-tablet.png" alt="Gitogram">
+        </template>
+        <template v-if="screenWidth <= 576">
+          <img src="../assets/device-mobile.png" alt="Gitogram">
+        </template>
+      </div>
+    </div>
+    <footer class="auth-page__footer">
+      <p>© Gitogram from Loftschool</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+import logo from "../components/logo";
+import xButton from "../components/xButton";
+
+export default {
+  name: "auth",
+
+  components: {
+    logo,
+    xButton,
+  },
+
+  data() {
+    return {
+      screenWidth: window.innerWidth
+    }
+  },
+
+  methods: {
+    updateScreenWidth() {
+      this.screenWidth = window.innerWidth;
+    }
+  },
+
+  created() {
+    window.addEventListener('resize', this.updateScreenWidth);
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.updateScreenWidth);
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.auth-page {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  
+  &__body {
+    flex: 1 0 100%;
+    padding: 0 10px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: end;
+  }
+
+  &__text-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin: 0 0 80px 0;
+    padding: 10px 0;
+    p {
+      margin: 12px 0 30px 0;
+    }
+
+    @media(max-width: 576px) {
+      order: 1;
+      flex: auto;
+      margin: 0;
+    }
+  }
+
+  &__img {
+    padding: 10px 0;
+
+    @media(max-width: 768px) {
+      padding: 10px 0 0 0;
+    }
+
+    @media(max-width: 576px) {
+      padding: 10px 0;
+      order: 0;
+      flex: auto;
+    }
+  }
+
+  &__footer {
+    width: 100%;
+    text-align: center;
+
+    padding: 32px 0;
+    font-size: 16px;
+    color: #999999;
+
+    border-top: 1px solid #c5c5c7;
+
+    @media(max-width: 576px) {
+      padding: 15px 0;
+    }
+  }
+}
+</style>
